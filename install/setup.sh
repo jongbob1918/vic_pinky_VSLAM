d7a44c08b9597d9172517013147e8200c3e95401
Jongmyung Kim <jongbob1918@gmail.com>
2025-09-28 12:53:54 +0900

    [update]

--- install/setup.sh
+++ install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/jongbob/dev_ws/vic_pinky_VSLAM/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/mac/dev_ws/vic_pinky_VSLAM/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
